ba7c88935dbe700f7fd787a2003baa915a971a44
imt-eiaz <imteiaz@gmail.com>
2026-04-04 10:50:05 +0100

    Dash added inthe title

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Namaste Byets Imtiaz" });
+  res.render("index", { title: "Namaste Byets - Imtiaz" });
 });
 
 module.exports = router;
